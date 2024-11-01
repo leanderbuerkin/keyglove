91a63442655c9355f1b9f408a26d0fa5c254f95c
lb617 <leander.buerkin@gmail.com>
2024-11-01 20:43:25 +0100

    data transmission seems to work with 1500 meassuremetns per second

--- src/get_acelerations.cpp
+++ src/get_acelerations.cpp
@@ -6,26 +6,30 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-#define ADDRESS_acceleration_rotation_1 0x68
-#define FIRST_DATA_REGISTER 0x3B
+#define ADDRESS_ACCELEROMETER_GYROSCOPE_1 0x68
 #define NUMBER_OF_DATA_REGISTERS 14
+#define MINIMAL_NUMBER_OF_DATA_REGISTERS 5
 #define POWER_MANAGMENT_REGISTER_ADDRESS 0x6B
 #define WAKE_UP_MESSAGE_TO_POWER_MANAGMENT_REGISTER 0
 
-BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic = NULL;
-bool deviceConnected = false;
+#define ACCELEROMETER_X_OUT_HIGH_BYTE 0x3B
+#define ACCELEROMETER_Y_OUT_HIGH_BYTE 0x3D
+#define ACCELEROMETER_Z_OUT_HIGH_BYTE 0x3F
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-int16_t acceleration_x;
-int16_t acceleration_y;
-int16_t acceleration_z;
-int16_t rotation_x;
-int16_t rotation_y;
-int16_t rotation_z;
-int16_t temperature;
+int acceleration_x;
+int acceleration_y;
+int acceleration_z;
+int rotation_x;
+int rotation_y;
+int rotation_z;
+int temperature;
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+bool device_connected = false;
 
 unsigned long lastSecond = 0;
 unsigned int dataPointCount = 0;
@@ -40,10 +44,30 @@ void setup_accelerometer_gyroscope(int sensor_address)
   Wire.endTransmission(true);
 }
 
-void read_accelerometer_gyroscope(int sensor_address)
+void read_accelerometer(int sensor_address)
 {
+  // 1114 meassurements per second with 14 registers (low and high bytes saved)
+  // 1866 meassurements per second with 6 registers (low and high bytes saved)
+  // 2030 meassurements per second with 5 registers (high bytes saved)
+  // 1302 meassurements per second with 3 registers (high bytes saved),
+  //      each of them individually called
+  // 400 kHz clock speed:
+  // 1126 meassurements per second with 14 registers (low and high bytes saved)
+  // 2120 meassurements per second with 5 registers (high bytes saved)
   Wire.beginTransmission(sensor_address);
-  Wire.write(FIRST_DATA_REGISTER);
+  Wire.write(ACCELEROMETER_X_OUT_HIGH_BYTE);
+  Wire.endTransmission(false);
+  Wire.requestFrom(sensor_address, MINIMAL_NUMBER_OF_DATA_REGISTERS, true);
+  acceleration_x = Wire.read();
+  Wire.read();
+  acceleration_y = Wire.read();
+  Wire.read();
+  acceleration_z = Wire.read();
+}
+
+void read_accelerometer_gyroscope(int sensor_address) {
+  Wire.beginTransmission(sensor_address);
+  Wire.write(ACCELEROMETER_X_OUT_HIGH_BYTE);
   Wire.endTransmission(false);
   Wire.requestFrom(sensor_address, NUMBER_OF_DATA_REGISTERS, true);
   acceleration_x = Wire.read() << 8 | Wire.read();
@@ -59,12 +83,12 @@ class MyServerCallbacks : public BLEServerCallbacks
 {
   void onConnect(BLEServer *pServer)
   {
-    deviceConnected = true;
+    device_connected = true;
   }
 
   void onDisconnect(BLEServer *pServer)
   {
-    deviceConnected = false;
+    device_connected = false;
   }
 };
 
@@ -96,16 +120,22 @@ void setup_bluetooth()
 void setup()
 {
   setup_bluetooth();
-  setup_accelerometer_gyroscope(ADDRESS_acceleration_rotation_1);
+  setup_accelerometer_gyroscope(ADDRESS_ACCELEROMETER_GYROSCOPE_1);
 }
 
 void loop()
 {
-  if (deviceConnected)
+  if (device_connected)
   {
-    read_accelerometer_gyroscope(ADDRESS_acceleration_rotation_1);
+    read_accelerometer(ADDRESS_ACCELEROMETER_GYROSCOPE_1);
 
     pCharacteristic->setValue(dataPointsPerSecond);
+    pCharacteristic->notify();
+    pCharacteristic->setValue(acceleration_x);
+    pCharacteristic->notify();
+    pCharacteristic->setValue(acceleration_y);
+    pCharacteristic->notify();
+    pCharacteristic->setValue(acceleration_z);
     pCharacteristic->notify();
     dataPointCount++;
   }
