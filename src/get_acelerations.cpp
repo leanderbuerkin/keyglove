c578da99e969546ce971148ade365d3e5b3d9046
lb617 <leander.buerkin@gmail.com>
2024-11-01 19:40:07 +0100

    working data submission via bluetooth

--- src/get_acelerations.cpp
+++ src/get_acelerations.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
 #define ADDRESS_acceleration_rotation_1 0x68
 #define FIRST_DATA_REGISTER 0x3B
 #define NUMBER_OF_DATA_REGISTERS 14
 #define POWER_MANAGMENT_REGISTER_ADDRESS 0x6B
 #define WAKE_UP_MESSAGE_TO_POWER_MANAGMENT_REGISTER 0
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+bool deviceConnected = false;
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 int16_t acceleration_x;
 int16_t acceleration_y;
@@ -15,7 +27,11 @@ int16_t rotation_y;
 int16_t rotation_z;
 int16_t temperature;
 
-void setup_mpu(int sensor_address)
+unsigned long lastSecond = 0;
+unsigned int dataPointCount = 0;
+unsigned int dataPointsPerSecond = 0;
+
+void setup_accelerometer_gyroscope(int sensor_address)
 {
   Wire.begin();
   Wire.beginTransmission(sensor_address);
@@ -24,13 +40,12 @@ void setup_mpu(int sensor_address)
   Wire.endTransmission(true);
 }
 
-void read_mpu(int sensor_address)
+void read_accelerometer_gyroscope(int sensor_address)
 {
   Wire.beginTransmission(sensor_address);
   Wire.write(FIRST_DATA_REGISTER);
   Wire.endTransmission(false);
   Wire.requestFrom(sensor_address, NUMBER_OF_DATA_REGISTERS, true);
-
   acceleration_x = Wire.read() << 8 | Wire.read();
   acceleration_y = Wire.read() << 8 | Wire.read();
   acceleration_z = Wire.read() << 8 | Wire.read();
@@ -40,27 +55,66 @@ void read_mpu(int sensor_address)
   rotation_z = Wire.read() << 8 | Wire.read();
 }
 
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+  }
+
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+  }
+};
+
+void setup_bluetooth()
+{
+  Serial.begin(115200);
+  BLEDevice::init("ESP32");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_NOTIFY);
+
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting for a client connection to notify...");
+}
+
 void setup()
 {
-  Serial.begin(115200); // Increase baud rate
-  setup_mpu(ADDRESS_acceleration_rotation_1);
+  setup_bluetooth();
+  setup_accelerometer_gyroscope(ADDRESS_acceleration_rotation_1);
 }
 
 void loop()
 {
-  read_mpu(ADDRESS_acceleration_rotation_1);
-  Serial.print(">acceleration_x:");
-  Serial.println(acceleration_x);
-  Serial.print(">acceleration_y:");
-  Serial.println(acceleration_y);
-  Serial.print(">acceleration_z:");
-  Serial.println(acceleration_z);
-  Serial.print(">rotation_x:");
-  Serial.println(rotation_x);
-  Serial.print(">rotation_y:");
-  Serial.println(rotation_y);
-  Serial.print(">rotation_z:");
-  Serial.println(rotation_z);
-  Serial.print(">temperature:");
-  Serial.println(temperature);
+  if (deviceConnected)
+  {
+    read_accelerometer_gyroscope(ADDRESS_acceleration_rotation_1);
+
+    pCharacteristic->setValue(dataPointsPerSecond);
+    pCharacteristic->notify();
+    dataPointCount++;
+  }
+  // Check if a second has passed
+  if (millis() - lastSecond >= 1000)
+  {
+    dataPointsPerSecond = dataPointCount;
+    dataPointCount = 0;
+    lastSecond = millis();
+  }
+  dataPointCount++;
 }
