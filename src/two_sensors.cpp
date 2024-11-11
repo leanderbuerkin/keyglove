9508e1ada16c5facbed6fed5013a6f71accbf497
lb617 <leander.buerkin@gmail.com>
2024-11-11 23:32:41 +0100

    working cleaned copy

--- src/two_sensors.cpp
+++ src/two_sensors.cpp
@@ -12,16 +12,17 @@ int first_mpu_SCL_pin = 22;
 
 int I2C_clock_frequency = 400000;
 
-Adafruit_MPU6050 mpu1;
-Adafruit_MPU6050 mpu2;
+Adafruit_MPU6050 first_accelerometer;
+Adafruit_MPU6050 second_accelerometer;
 
-void initialize_mpu(TwoWire I2C_bus) {
-    if (!mpu1.begin(0x68, &I2C_bus))
+void initialize_mpu(TwoWire &I2C_bus) {
+    Serial.println("Initializing MPU6050 sensor...");
+    if (!first_accelerometer.begin(0x68, &I2C_bus))
     {
-        Serial.println("Failed to find MPU6050 chip");
-        while (1) delay(10);
+        Serial.println("MPU6050 chip not found");
+        while (1);
     }
-    Serial.println("MPU6050 Found!");
+    Serial.println("MPU6050 found");
 }
 
 void setup() {
@@ -29,40 +30,18 @@ void setup() {
     while (!Serial) delay(10);
 
     first_I2C_bus.begin(first_mpu_SDA_pin, first_mpu_SCL_pin, I2C_clock_frequency);
-
     second_I2C_bus.begin(second_mpu_SDA_pin, second_mpu_SCL_pin, I2C_clock_frequency);
 
-    Serial.println("Initializing MPU6050 sensors...");
+    initialize_mpu(first_I2C_bus);
+    initialize_mpu(second_I2C_bus);
 
-    // Initialize the first MPU6050
-    if (!mpu1.begin(0x68, &first_I2C_bus))
-    {
-        Serial.println("Failed to find first MPU6050 chip");
-        while (1)
-        {
-            delay(10);
-        }
-    }
-    Serial.println("First MPU6050 Found!");
+    first_accelerometer.setAccelerometerRange(MPU6050_RANGE_8_G);
+    first_accelerometer.setGyroRange(MPU6050_RANGE_500_DEG);
+    first_accelerometer.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-    // Initialize the second MPU6050
-    if (!mpu2.begin(0x68, &second_I2C_bus))
-    {
-        Serial.println("Failed to find second MPU6050 chip");
-        while (1)
-        {
-            delay(10);
-        }
-    }
-    Serial.println("Second MPU6050 Found!");
-
-    mpu1.setAccelerometerRange(MPU6050_RANGE_8_G);
-    mpu1.setGyroRange(MPU6050_RANGE_500_DEG);
-    mpu1.setFilterBandwidth(MPU6050_BAND_21_HZ);
-
-    mpu2.setAccelerometerRange(MPU6050_RANGE_8_G);
-    mpu2.setGyroRange(MPU6050_RANGE_500_DEG);
-    mpu2.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    second_accelerometer.setAccelerometerRange(MPU6050_RANGE_8_G);
+    second_accelerometer.setGyroRange(MPU6050_RANGE_500_DEG);
+    second_accelerometer.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
     delay(100);
 }
@@ -71,11 +50,11 @@ void loop()
 {
     // Read data from the first MPU6050
     sensors_event_t a1, g1, temp1;
-    mpu1.getEvent(&a1, &g1, &temp1);
+    first_accelerometer.getEvent(&a1, &g1, &temp1);
 
     // Read data from the second MPU6050
     sensors_event_t a2, g2, temp2;
-    mpu2.getEvent(&a2, &g2, &temp2);
+    second_accelerometer.getEvent(&a2, &g2, &temp2);
 
     Serial.print(">acceleration_x1:");
     Serial.println(a1.acceleration.x);
