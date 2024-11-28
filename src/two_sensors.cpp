1140ba1842372069221f151a5996451cfd27f134
lb617 <leander.buerkin@gmail.com>
2024-11-28 22:16:37 +0100

    changed to z-axis and added conversion to signed int_8

--- src/two_sensors.cpp
+++ src/two_sensors.cpp
@@ -1,20 +1,24 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+///// ToDo: Reads Z data register now, document!!
+
 #define MPU_ADDRESS 0x68
 #define POWER_MANAGMENT_REGISTER_ADDRESS 0x6B
 #define WAKE_UP_MESSAGE_TO_POWER_MANAGMENT 0
 #define FIRST_DATA_REGISTER_ADDRESS 0x3B
+#define Z_ACCELERATION_DATA_REGISTER_ADDRESS 0x3F
 #define NUMBER_OF_NEEDED_REGISTERS 1
 #define I2C_CLOCK_FREQUENCY 400000
 #define FIRST_MPU_SDA_PIN 18
 #define FIRST_MPU_SCL_PIN 19
 #define SECOND_MPU_SDA_PIN 21
 #define SECOND_MPU_SCL_PIN 22
-#define SERIAL_TRANSMISSION_START_MESSAGE "go"
+#define SERIAL_TRANSMISSION_START_MESSAGE_SENSOR_1 "ONE"
+#define SERIAL_TRANSMISSION_START_MESSAGE_SENSOR_2 "TWO"
 
-TwoWire first_I2C_bus = TwoWire(0);
-TwoWire second_I2C_bus = TwoWire(1);
+TwoWire I2C_bus_1 = TwoWire(0);
+TwoWire I2C_bus_2 = TwoWire(1);
 
 void setup_mpu(TwoWire &I2C_bus)
 {
@@ -27,25 +31,38 @@ void setup_mpu(TwoWire &I2C_bus)
 void setup()
 {
     Serial.begin(921600);
-    first_I2C_bus.begin(FIRST_MPU_SDA_PIN, FIRST_MPU_SCL_PIN, I2C_CLOCK_FREQUENCY);
-    second_I2C_bus.begin(SECOND_MPU_SDA_PIN, SECOND_MPU_SCL_PIN, I2C_CLOCK_FREQUENCY);
-    setup_mpu(first_I2C_bus);
-    setup_mpu(second_I2C_bus);
+    I2C_bus_1.begin(FIRST_MPU_SDA_PIN, FIRST_MPU_SCL_PIN, I2C_CLOCK_FREQUENCY);
+    I2C_bus_2.begin(SECOND_MPU_SDA_PIN, SECOND_MPU_SCL_PIN, I2C_CLOCK_FREQUENCY);
+    setup_mpu(I2C_bus_1);
+    setup_mpu(I2C_bus_2);
 }
 
-void request_acceleration_x_high_byte(TwoWire &I2C_bus)
+void request_one_acceleration_high_byte(TwoWire &I2C_bus)
 {
     I2C_bus.beginTransmission(MPU_ADDRESS);
-    I2C_bus.write(FIRST_DATA_REGISTER_ADDRESS);
+    I2C_bus.write(Z_ACCELERATION_DATA_REGISTER_ADDRESS);
     I2C_bus.endTransmission(false);
     I2C_bus.requestFrom(MPU_ADDRESS, NUMBER_OF_NEEDED_REGISTERS);
 }
 
+void serial_to_teleplot()
+{
+    Serial.print(">Sensor1:");
+    Serial.println(static_cast<int8_t>(I2C_bus_1.read()));
+    Serial.print(">Sensor2:");
+    Serial.println(static_cast<int8_t>(I2C_bus_2.read()));
+}
+
+void serial_to_python()
+{
+    Serial.print(static_cast<int8_t>(I2C_bus_1.read()));
+    Serial.print(" ");
+    Serial.println(static_cast<int8_t>(I2C_bus_2.read()));
+}
+
 void loop()
 {
-    Serial.println(SERIAL_TRANSMISSION_START_MESSAGE);
-    request_acceleration_x_high_byte(first_I2C_bus);
-    Serial.println(first_I2C_bus.read());
-    request_acceleration_x_high_byte(second_I2C_bus);
-    Serial.println(second_I2C_bus.read());
+    request_one_acceleration_high_byte(I2C_bus_1);
+    request_one_acceleration_high_byte(I2C_bus_2);
+    serial_to_python();
 }
