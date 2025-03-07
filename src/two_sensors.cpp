238f33e51eca11bbaea7d34d4c5e61f815fa79ad
lb617 <leander.buerkin@gmail.com>
2025-03-07 12:11:03 +0100

    Added Lowpassfilter

--- src/two_sensors.cpp
+++ src/two_sensors.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <chrono>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
 #define MPU_ADDRESS 0x68
 #define POWER_MANAGMENT_REGISTER_ADDRESS 0x6B
@@ -17,21 +18,20 @@
 TwoWire I2C_bus_1 = TwoWire(0);
 TwoWire I2C_bus_2 = TwoWire(1);
 
-void setup_mpu(TwoWire &I2C_bus)
-{
-    I2C_bus.beginTransmission(MPU_ADDRESS);
-    I2C_bus.write(POWER_MANAGMENT_REGISTER_ADDRESS);
-    I2C_bus.write(WAKE_UP_MESSAGE_TO_POWER_MANAGMENT);
-    I2C_bus.endTransmission(true);
-}
+Adafruit_MPU6050 mpu1;
+Adafruit_MPU6050 mpu2;
 
 void setup(void)
 {
     Serial.begin(921600);
     I2C_bus_1.begin(MPU_1_SDA_PIN, MPU_1_SCL_PIN, I2C_CLOCK_FREQUENCY);
     I2C_bus_2.begin(MPU_2_SDA_PIN, MPU_2_SCL_PIN, I2C_CLOCK_FREQUENCY);
-    setup_mpu(I2C_bus_1);
-    setup_mpu(I2C_bus_2);
+
+    mpu1.begin(MPU_ADDRESS, &I2C_bus_1);
+    mpu2.begin(MPU_ADDRESS, &I2C_bus_2);
+
+    mpu1.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    mpu2.setFilterBandwidth(MPU6050_BAND_5_HZ);
 }
 
 void request_data_from_sensor(TwoWire &I2C_bus)
@@ -47,14 +47,14 @@ void send_via_serial_to_teleplot(void)
     request_data_from_sensor(I2C_bus_1);
     Serial.print(">Sensor1:");
     // use the following for more continous values, without it is faster
-    // Serial.println(static_cast<int8_t>(I2C_bus_1.read()));
-    Serial.println(I2C_bus_1.read());
+    Serial.println(static_cast<int8_t>(I2C_bus_1.read()));
+    // Serial.println(static_cast<int8_t>(acceleration_sensor_1));
 
     request_data_from_sensor(I2C_bus_2);
     Serial.print(">Sensor2:");
     // use the following for more continous values, without it is faster
-    // Serial.println(static_cast<int8_t>(I2C_bus_2.read()));
-    Serial.println(I2C_bus_2.read());
+    Serial.println(static_cast<int8_t>(I2C_bus_2.read()));
+    // Serial.println(static_cast<int8_t>(acceleration_sensor_2));
 }
 
 void send_via_serial_to_python(void)
