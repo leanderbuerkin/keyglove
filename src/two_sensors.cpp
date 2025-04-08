402f1a0dd5f295e4754b2f42d06f2a8c8821afd5
lb617 <leander.buerkin@gmail.com>
2025-04-08 15:29:14 +0200

    four sensors

--- src/two_sensors.cpp
+++ src/two_sensors.cpp
@@ -4,6 +4,7 @@
 #include <Adafruit_Sensor.h>
 
 #define MPU_ADDRESS 0x68
+#define MPU_ADDRESS_2 0x69
 #define POWER_MANAGMENT_REGISTER_ADDRESS 0x6B
 #define WAKE_UP_MESSAGE_TO_POWER_MANAGMENT 0
 #define X_ACCELERATION_REGISTER_ADDRESS 0x3B
@@ -20,6 +21,8 @@ TwoWire I2C_bus_2 = TwoWire(1);
 
 Adafruit_MPU6050 mpu1;
 Adafruit_MPU6050 mpu2;
+Adafruit_MPU6050 mpu3;
+Adafruit_MPU6050 mpu4;
 
 void setup(void)
 {
@@ -29,39 +32,58 @@ void setup(void)
 
     mpu1.begin(MPU_ADDRESS, &I2C_bus_1);
     mpu2.begin(MPU_ADDRESS, &I2C_bus_2);
+    mpu3.begin(MPU_ADDRESS_2, &I2C_bus_1);
+    mpu4.begin(MPU_ADDRESS_2, &I2C_bus_2);
 
     mpu1.setFilterBandwidth(MPU6050_BAND_5_HZ);
     mpu2.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    mpu3.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    mpu4.setFilterBandwidth(MPU6050_BAND_5_HZ);
 }
 
-void request_data_from_sensor(TwoWire &I2C_bus)
+void request_data_from_sensor(TwoWire &I2C_bus, bool second_sensor)
 {
-    I2C_bus.beginTransmission(MPU_ADDRESS);
-    I2C_bus.write(X_ACCELERATION_REGISTER_ADDRESS);
-    I2C_bus.endTransmission(false);
-    I2C_bus.requestFrom(MPU_ADDRESS, NUMBER_OF_NEEDED_REGISTERS);
+    if (second_sensor)
+    {
+        I2C_bus.beginTransmission(MPU_ADDRESS_2);
+        I2C_bus.write(X_ACCELERATION_REGISTER_ADDRESS);
+        I2C_bus.endTransmission(false);
+        I2C_bus.requestFrom(MPU_ADDRESS_2, NUMBER_OF_NEEDED_REGISTERS);
+    }
+    else {
+        I2C_bus.beginTransmission(MPU_ADDRESS);
+        I2C_bus.write(X_ACCELERATION_REGISTER_ADDRESS);
+        I2C_bus.endTransmission(false);
+        I2C_bus.requestFrom(MPU_ADDRESS, NUMBER_OF_NEEDED_REGISTERS);
+    }
+    
 }
 
 void send_via_serial_to_teleplot(void)
 {
-    request_data_from_sensor(I2C_bus_1);
+    request_data_from_sensor(I2C_bus_1, false);
     Serial.print(">Sensor1:");
     // use the following for more continous values, without it is faster
     Serial.println(static_cast<int8_t>(I2C_bus_1.read()));
     // Serial.println(static_cast<int8_t>(acceleration_sensor_1));
 
-    request_data_from_sensor(I2C_bus_2);
+    request_data_from_sensor(I2C_bus_2, false);
     Serial.print(">Sensor2:");
     // use the following for more continous values, without it is faster
     Serial.println(static_cast<int8_t>(I2C_bus_2.read()));
     // Serial.println(static_cast<int8_t>(acceleration_sensor_2));
-}
 
-void send_via_serial_to_python(void)
-{
-    Serial.print(static_cast<int8_t>(I2C_bus_1.read()));
-    Serial.print(" ");
+    request_data_from_sensor(I2C_bus_1, true);
+    Serial.print(">Sensor3:");
+    // use the following for more continous values, without it is faster
+    Serial.println(static_cast<int8_t>(I2C_bus_1.read()));
+    // Serial.println(static_cast<int8_t>(acceleration_sensor_1));
+
+    request_data_from_sensor(I2C_bus_2, true);
+    Serial.print(">Sensor4:");
+    // use the following for more continous values, without it is faster
     Serial.println(static_cast<int8_t>(I2C_bus_2.read()));
+    // Serial.println(static_cast<int8_t>(acceleration_sensor_2));
 }
 
 void loop(void)
