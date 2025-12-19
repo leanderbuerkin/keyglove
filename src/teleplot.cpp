2fb56b01477f0bcf0e71fa372395e29ea21fd8b4
Leander Bürkin <leander.buerkin@gmail.com>
2025-12-19 17:10:00 +0100

    New working solution

--- src/teleplot.cpp
+++ src/teleplot.cpp
@@ -3,14 +3,18 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 
+// ToDo: sort this file
+// ToDo: Add watchdog
+
+#define WDT_TIMEOUT 5  // seconds before watchdog reset
 #define MPU_ADDRESS_1 0x68
 #define MPU_ADDRESS_2 0x69
-#define POWER_MANAGMENT_REGISTER_ADDRESS 0x6B
-#define WAKE_UP_MESSAGE_TO_POWER_MANAGMENT 0
+#define POWER_MANAGEMENT_REGISTER_ADDRESS 0x6B
+#define WAKE_UP_MESSAGE_TO_POWER_MANAGEMENT 0
 #define X_ACCELERATION_REGISTER_ADDRESS 0x3B
 #define Z_ACCELERATION_REGISTER_ADDRESS 0x3F
 #define NUMBER_OF_NEEDED_REGISTERS 1
-#define I2C_CLOCK_FREQUENCY 400000
+#define I2C_CLOCK_FREQUENCY 100000
 #define MPU_1_SDA_PIN 18
 #define MPU_1_SCL_PIN 19
 #define MPU_2_SDA_PIN 21
@@ -24,22 +28,51 @@ Adafruit_MPU6050 mpu2;
 Adafruit_MPU6050 mpu3;
 Adafruit_MPU6050 mpu4;
 
+static unsigned long lastUpdate;
+
 void setup(void)
 {
-
-    Serial.begin(921600);
+    Serial.begin(9600);
+    lastUpdate = millis();
     I2C_bus_1.begin(MPU_1_SDA_PIN, MPU_1_SCL_PIN, I2C_CLOCK_FREQUENCY);
     I2C_bus_2.begin(MPU_2_SDA_PIN, MPU_2_SCL_PIN, I2C_CLOCK_FREQUENCY);
 
-    mpu1.begin(MPU_ADDRESS_1, &I2C_bus_1);
-    mpu2.begin(MPU_ADDRESS_1, &I2C_bus_2);
-    mpu3.begin(MPU_ADDRESS_2, &I2C_bus_1);
-    mpu4.begin(MPU_ADDRESS_2, &I2C_bus_2);
+    if (!mpu1.begin(MPU_ADDRESS_1, &I2C_bus_1)) {
+        Serial.println("Failed to initialize MPU1!");
+    }
+    if (!mpu2.begin(MPU_ADDRESS_1, &I2C_bus_2)) {
+        Serial.println("Failed to initialize MPU2!");
+    }
+    if (!mpu3.begin(MPU_ADDRESS_2, &I2C_bus_1)) {
+        Serial.println("Failed to initialize MPU3!");
+    }
+    if (!mpu4.begin(MPU_ADDRESS_2, &I2C_bus_2)) {
+        Serial.println("Failed to initialize MPU4!");
+    }
 
     mpu1.setFilterBandwidth(MPU6050_BAND_5_HZ);
     mpu2.setFilterBandwidth(MPU6050_BAND_5_HZ);
     mpu3.setFilterBandwidth(MPU6050_BAND_5_HZ);
     mpu4.setFilterBandwidth(MPU6050_BAND_5_HZ);
+    mpu1.setHighPassFilter(MPU6050_HIGHPASS_DISABLE);
+    mpu2.setHighPassFilter(MPU6050_HIGHPASS_DISABLE);
+    mpu3.setHighPassFilter(MPU6050_HIGHPASS_DISABLE);
+    mpu4.setHighPassFilter(MPU6050_HIGHPASS_DISABLE);
+
+    if (I2C_bus_1.requestFrom(MPU_ADDRESS_1, NUMBER_OF_NEEDED_REGISTERS) != NUMBER_OF_NEEDED_REGISTERS) {
+        Serial.println("I2C read error!");
+    }
+    if (I2C_bus_2.requestFrom(MPU_ADDRESS_1, NUMBER_OF_NEEDED_REGISTERS) != NUMBER_OF_NEEDED_REGISTERS) {
+        Serial.println("I2C read error!");
+    }
+    if (I2C_bus_1.requestFrom(MPU_ADDRESS_2, NUMBER_OF_NEEDED_REGISTERS) != NUMBER_OF_NEEDED_REGISTERS) {
+        Serial.println("I2C read error!");
+    }
+    if (I2C_bus_2.requestFrom(MPU_ADDRESS_2, NUMBER_OF_NEEDED_REGISTERS) != NUMBER_OF_NEEDED_REGISTERS) {
+        Serial.println("I2C read error!");
+    }
+
+
 }
 
 void request_data_from_sensor(TwoWire &I2C_bus, int mpu_address)
@@ -53,23 +86,26 @@ void request_data_from_sensor(TwoWire &I2C_bus, int mpu_address)
 void serial_print_sensor_data(TwoWire &I2C_bus, int mpu_address)
 {
     request_data_from_sensor(I2C_bus, mpu_address);
-    // use static_cast<int8_t> for more continous values, without it is faster
+    // use static_cast<int8_t> for more continuous values, without it is faster
     Serial.println(static_cast<int8_t>(I2C_bus.read()));
 }
 
 void serial_print_for_debugging_with_teleplot(void)
 {
     Serial.print(">Sensor 1:");
-    serial_print_sensor_data(I2C_bus_1, MPU_ADDRESS_1);
-    Serial.print(">Sensor 2:");
-    serial_print_sensor_data(I2C_bus_2, MPU_ADDRESS_1);
-    Serial.print(">Sensor 3:");
     serial_print_sensor_data(I2C_bus_1, MPU_ADDRESS_2);
-    Serial.print(">Sensor 4:");
+    Serial.print(">Sensor 2:");
+    serial_print_sensor_data(I2C_bus_1, MPU_ADDRESS_1);
+    Serial.print(">Sensor 3:");
     serial_print_sensor_data(I2C_bus_2, MPU_ADDRESS_2);
+    Serial.print(">Sensor 4:");
+    serial_print_sensor_data(I2C_bus_2, MPU_ADDRESS_1);
 }
 
 void loop(void)
 {
-    serial_print_for_debugging_with_teleplot();
+    if (millis() - lastUpdate >= 10) {
+        serial_print_for_debugging_with_teleplot();
+        lastUpdate = millis();
+    }
 }
